a696f35b422d582dc1b34ec17b6f685ee353d781
Andrew Eames <ajerxs@gmail.com>
2021-07-05 22:01:15 -0400

    Done.

---
+++ app/models/super_hero.rb
@@ -0,0 +1,17 @@
+class SuperHero
+    @@all = []
+
+    attr_reader :name, :power, :bio
+
+    def initialize(params)
+        @name = params[:name]
+        @power = params[:power]
+        @bio = params[:bio]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
---
+++ app/models/team.rb
@@ -0,0 +1,16 @@
+class Team
+    @@all = []
+
+    attr_reader :name, :motto
+
+    def initialize(params)
+        @name = params[:name]
+        @motto = params[:motto]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
